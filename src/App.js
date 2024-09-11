775acabd9bbd60190f70e17d61d461c1e1abf0ea
ElbieLi <zli3@seattleu.edu>
2024-09-11 16:52:09 -0400

    Add styling to stage 3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 
 
 const customers = [
@@ -27,25 +28,27 @@ function App() {
 
   return (
     <div>
-      <h1>Customer List</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Pass</th>
-          </tr>
-        </thead>
-        <tbody>
-          {customers.map((customer, index) => (
-            <tr key={index} onClick={handleListClick}>
-              <td>{customer.name}</td>
-              <td>{customer.email}</td>
-              <td>{customer.password}</td>
+      <div className="boxed">
+        <h4>Customer List</h4>
+        <table id="customer-list">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Pass</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {customers.map((customer, index) => (
+              <tr key={index} onClick={handleListClick}>
+                <td>{customer.name}</td>
+                <td>{customer.email}</td>
+                <td>{customer.password}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
 
       <CustomerForm
         onDeleteClick={onDeleteClick}
@@ -59,27 +62,40 @@ function App() {
 function CustomerForm({onDeleteClick, onSaveClick, onCancelClick}){
   return(
     <div>
-      <h2>Update</h2>
-      <form>
-        <label>
-          Name:
-          <input type="text" value="Jack Jackson" readOnly />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input type="email" value="jackj@abc.com" readOnly />
-        </label>
-        <br />
-        <label>
-          Pass:
-          <input type="password" value="jackj" readOnly />
-        </label>
-        <br />
-        <button type="button" onClick={onDeleteClick}>Delete</button>
-        <button type="button" onClick={onSaveClick}>Save</button>
-        <button type="button" onClick={onCancelClick}>Cancel</button>
-      </form>
+      <div className="boxed">
+        <h4>Update</h4>
+        <form>
+          <table id="customer-add-update">
+            <tbody>
+              <tr>
+                <td className={'label'}>Name:</td>
+                <td>
+                  <input type="text" name="name" placeholder="Customer Name"/>
+                </td>
+              </tr>
+              <tr>
+                <td className={'label'}>Email:</td>
+                <td>
+                  <input type="email" name="email" placeholder="name@company.com"/>
+                </td>
+              </tr>
+              <tr>
+                <td className={'label'}>Pass:</td>
+                <td>
+                  <input type="text" name="password" placeholder="password"/>
+                </td>
+              </tr>
+            </tbody>
+            <tr className="button-bar">
+              <td colSpan="2">
+                <button type="button" onClick={onDeleteClick}>Delete</button>
+                <button type="button" onClick={onSaveClick}>Save</button>
+                <button type="button" onClick={onCancelClick}>Cancel</button>
+              </td>
+            </tr>
+          </table>
+        </form>
+      </div>
     </div>
   );
 }
